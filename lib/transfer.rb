7bfa9571c8704723f45b3d355527330af5ce5a59
Justin Langlinais <justinclanglinais@gmail.com>
2020-04-23 15:37:35 -0700

    justin commit #1

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,24 @@
 class Transfer
-  # your code here
+
+  attr_accessor :status
+  attr_reader :sender, :receiver, :amount
+
+  def initialize(sender,receiver,amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    @sender.valid? && @receiver.valid?
+  end
+
+  def execute_transaction
+    
+    @receiver.deposit(@amount)
+    @sender.deposit(-(@amount))
+    @status = "complete"
+  end
+
 end
