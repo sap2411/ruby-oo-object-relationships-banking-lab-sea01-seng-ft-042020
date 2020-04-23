a67a80ee3b105f39e2f0b9323defe2ee13019372
sap2411 <steven.parsons.us@gmail.com>
2020-04-23 15:46:02 -0700

    wrong

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,32 @@
 class BankAccount
+    attr_accessor :balance, :status
+    attr_reader :name
+    @@all = []
 
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+        self.save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def deposit(amount)
+        @balance = @balance + amount
+    end
+
+    def display_balance
+        "Your balance is $#{1000}."
+    end
+
+    def valid?
+        self.status == "open" && self.balance > 0 ? true : false
+    end
+
+    def close_account
+        @@all 
+    end
 end
